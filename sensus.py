79151562d4fd7f7976e88021d5a58fd016e76814
SitiZahraSifa11 <160223744+SitiZahraSifa11@users.noreply.github.com>
2024-06-28 14:18:32 +0700

    Add files via upload

---
+++ sensus.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+df = pd.read_csv('sensus.xlsx - Sheet1.csv')
+
+# lakiLaki= df[df['JENIS\rKELAMIN'] == 'L']
+# jumlah= len(lakiLaki)
+# print(lakiLaki)
+# print(jumlah)
+
+# perempuan= df[df['JENIS\rKELAMIN'] == 'P']
+# jumlahP=len(perempuan)
+# print(perempuan)
+# print(jumlahP)
+
+# umur=df[df['USIA'] > 50]
+# print(umur)
+
+# pindah=df[df['KET.'] == 'pindah memilih']
+# semuanya=len(pindah)
+# print(pindah)
+# print(semuanya)
+
+
